e6cb107590ae92a0f1118f469ec5845d267ff264
crrojas88 <carlorrojas@gmail.com>
2020-11-19 10:12:42 -0800

    not passing tests, but functionality works

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Add your code here
+
+function submitData(name, email) {
+
+    let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name: name,
+    email: email
+  })
+    };
+
+
+     fetch("http://localhost:3000/users", configObj)
+     .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+          console.log(object)
+        /*find the new id and append it to the DOM */
+        appendToDom(object.id)
+      })
+      .catch(function(error) {
+        /*expect to receive an object on error with a property, message*/
+      })
+
+}
+
+
+function appendToDom(id) {
+    const body = document.querySelector('body')
+    body.innerHTML = id
+}
+
+function appendError(message) {
+    const errorMsg = document.createElement('p')
+    errorMsg.innerHTML = message
+}
+submitData('rod', 'blabbla@gmaiol.com')
